2e004085e0977d07a0f9d40316ad1a72f7ff00a3
Hongrun <hongrunz@andrew.cmu.edu>
2021-10-09 13:02:32 -0400

    genre plot done

--- app.py
+++ app.py
@@ -1,39 +1,35 @@
 import streamlit as st
 import sqlite3
-import seaborn as sns
+import pandas as pd
+import plotly.figure_factory as ff
 import numpy as np
 
 con = sqlite3.connect("database.sqlite")
 cur = con.cursor()
 
-sns.set_theme()
-
+st.set_page_config(layout="wide")
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# TODO: change layout
-row1_1, row1_2 = st.columns((2,3))
+st.title("Pitchfork Reviews from Jan 5, 1999 to Jan 8, 2017")
+st.write("Examining how music review scores vary across genres and across years")
 
-with row1_1:
-    st.title("Pitchfork Reviews from Jan 5, 1999 to Jan 8, 2017")
-
-with row1_2:
-    st.write("Examining how music review scores vary across genres and across years")
-
-row2_1, row2_2, row2_3, row2_4 = st.columns((2,1,1,1))
-
-with row2_1:
-    # TODO: Add different genres to the same plot with different hues
-    option = st.selectbox(
-      "What genre would you like to see?",
-      ("rap", "electronic", "rock", "experimental", "metal", "jazz", "pop/r&b", "folk/country", "")
-    )
-    x = []
-    query = "SELECT score FROM reviews INNER JOIN genres ON reviews.reviewid = genres.reviewid WHERE genre IS \"" + option + "\";"
-    for row in cur.execute(query):
-      x.append(float(row[0]))
-
-    ax = sns.histplot(x)
-    st.pyplot()
+# Part I genre score distplot
+genres = st.multiselect(
+  "What genre would you like to see? Use the multislector below to compare different score distribution across genres",
+  ("rap", "electronic", "rock", "experimental", "metal", "jazz", "pop/r&b", "folk/country", "global"),
+  ["electronic"]
+)
+scores = []
+for g in genres:
+  query = "SELECT score FROM reviews INNER JOIN genres ON reviews.reviewid = genres.reviewid WHERE genre IS \"" + g + "\";"
+  genre_scores = []
+  for row in cur.execute(query):
+    genre_scores.append(row[0])
+  scores.append(genre_scores)
+# score_genres = pd.read_sql_query('SELECT score, genre FROM reviews INNER JOIN genres ON reviews.reviewid = genres.reviewid', con)
+fig = ff.create_distplot(scores, genres, bin_size=.2)
+fig.update_layout(width=1300, height=800)
+st.plotly_chart(fig)
 
 # TODO: Add different years
 
